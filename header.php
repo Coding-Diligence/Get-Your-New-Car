2db611a7effe8b7d3339818232cae9165c24611b
sarahlte <sarah_leconte@msn.com>
2024-03-08 14:27:28 +0100

    logout

--- header.php
+++ header.php
@@ -6,15 +6,25 @@
       <a class="title" href="index.php">Cardiction</a>
     </div>
     <div>
-      <button class="search-submit">
-        <a class="fin" href="login.php">Connexion</a>
-      </button>
-      <button class="search-submit">
-        <a class="fin" href="interface.php">Add Car</a>
-      </button>
-      <button class="search-submit">
-        <a class="fin" href="message.php">Messagerie</a>
-      </button>
+      <?php if(isset($_SESSION['connexion']) && $_SESSION['connexion'] == 'connected'):?>
+        <button class="search-submit">
+          <a class="fin" href="./controllers/logout_controller.php">deconnexion</a>          
+        </button>
+        <button class="search-submit">
+          <a class="fin" href="interface.php">Add Car</a>
+        </button>
+        <button class="search-submit">
+          <a class="fin" href="message.php">Messagerie</a>
+        </button>
+        <?php else : ?>
+          <button class="search-submit">
+            <a class="fin" href="?page=register">inscription</a>
+          </button>
+          <button class="search-submit">
+            <a class="fin" href="login.php">Connexion</a>
+          </button>
+      <?php endif; ?>
+
     </div>
   </div>
 </nav>
