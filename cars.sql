e6b3bfb645bb058b34fea0b01998665ca02e805d
Coding-Diligence <kilian.poussard.pro@gmail.com>
2024-03-08 12:02:25 +0100

    dtb

--- cars.sql
+++ cars.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Mar 08, 2024 at 10:27 AM
+-- Generation Time: Mar 08, 2024 at 11:00 AM
 -- Server version: 8.0.31
 -- PHP Version: 8.2.0
 
@@ -37,15 +37,19 @@ CREATE TABLE IF NOT EXISTS `cars` (
   `state` enum('new','used','damaged') DEFAULT NULL,
   `image_path` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `cars`
 --
 
 INSERT INTO `cars` (`id`, `name`, `price`, `color`, `kilometrage`, `state`, `image_path`) VALUES
-(1, 'Subaru', '9000.00', 'blue', 10000, 'used', 'assets/voiture1.jpg'),
-(2, 'Gtrr', '18000.00', 'white', 5000, 'new', 'assets/voiture2.jpg');
+(20, 'Honda Type R 1998–2001', '29000.00', 'white', 1000, 'new', 'assets/voiture5.jpg'),
+(19, 'Mitsubishi', '22000.00', 'grey', 148600, 'used', 'assets/voiture4.jpg'),
+(18, 'Dirt Car', '2500.00', 'blue', 17000, 'used', 'assets/voiture3.jpg'),
+(17, 'Gtrr', '18000.00', 'white', 15000, 'used', 'assets/voiture2.jpg'),
+(16, 'Subaru', '9000.00', 'blue', 3000, 'new', 'assets/voiture1.jpg'),
+(21, 'Mitsubishi Evo', '1700.00', 'white', 0, 'new', 'assets/voiture6.jpg');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
