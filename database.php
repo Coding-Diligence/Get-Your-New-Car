2ca21af130930521fc6eeb1deb4db570ba2679c6
Coding-Diligence <kilian.poussard.pro@gmail.com>
2024-03-08 16:00:34 +0100

    database

--- database.php
+++ database.php
@@ -1,44 +1,4 @@
 <?php
-switch ($_SERVER['SERVER_NAME']) {
-    case '127.0.0.1':
-    case 'localhost':
-        define('ENVIRONMENT', 'developpement');
-
-        $paramsServer = array(
-            'server'    => "localhost",
-            'database'  => "car_management",
-            'username'  => "root",
-            'password'  => "",
-            'port' => 3306,
-        );
-
-        define('PREFIX', '');
-        define('DNS', 'https://car.test/' . PREFIX);
-        define('HOME', $_SERVER['CONTEXT_DOCUMENT_ROOT']);
-
-        define('PATH_FILES', "files/");
-
-        break;
-
-    default:
-        define('ENVIRONMENT', 'production');
-
-        $paramsServer = array(
-            'server' => "mysql.db",
-            'database' => "car_management",
-            'username' => "",
-            'password' => "",
-            'port' => 3306
-        );
-
-        define('PREFIX', '');
-        define('DNS', 'https://www.portfolio.fr/' . PREFIX);
-        define('HOME', $_SERVER['DOCUMENT_ROOT']);
-
-        define('PATH_FILES', "files/");
-        exit;
-        
-}
 session_start();
 if (PHP_OS == 'WINNT') {
     try {
@@ -61,28 +21,3 @@ if (PHP_OS == 'WINNT') {
         $_SESSION['db'] = 'Error!';
     }
 }
-/* $cars = [
-    ['Subaru', 9000, 'blue', 3000, 'new', 'assets/voiture1.jpg'],
-    ['Gtrr', 18000, 'white', 15000, 'used', 'assets/voiture2.jpg'],
-    ['Dirt Car', 2500, 'blue', 17000, 'used','assets/voiture3.jpg'],
-    ['Mitsubishi', 22000, 'grey', 148600, 'used','assets/voiture4.jpg'],
-    ['Honda Type R 1998–2001', 29000, 'white', 1000, 'new','assets/voiture5.jpg'],
-    ['Mitsubishi Evo', 1700, 'white', 0, 'new','assets/voiture6.jpg']
-];
-
-foreach ($cars as $car) {
-    $name = $car[0];
-    $price = $car[1];
-    $color = $car[2];
-    $kilometrage = $car[3];
-    $state = $car[4];
-    $img = $car[5];
-
-    //SQL INSERT statement
-    $sql = "INSERT INTO cars (name, price, color, kilometrage, state, image_path) VALUES ('$name', $price, '$color', '$kilometrage', '$state', '$img')";
-
-    //Execute the SQL statement
-    if ($bdLink->query($sql) === TRUE) {
-        echo "New record created successfully";
-    }
-} */
