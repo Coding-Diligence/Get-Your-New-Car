69225aa097129efdf5de8c4aa28b46119ef3260d
Coding-Diligence <kilian.poussard.pro@gmail.com>
2024-03-08 10:34:10 +0100

    searchbar

--- script.js
+++ script.js
@@ -20,54 +20,9 @@ function includeHTML() {
     }
   }
 }
-const backup = [
-  { name: 'Subaru',price: 9000, color: "blue" }, 
-  { name: 'Gtrr',price: 18000, color: "white" },
-  { name: 'Dirt Car',price: 2500, color: "blue" },
-  { name: 'Mitsubishi',price: 22000, color: "grey" }
+const car = [
+  { name: 'Subaru',price: 9000, color: "blue", img: "assets/voiture1.jpg" }, 
+  { name: 'Gtrr',price: 18000, color: "white", img: "assets/voiture2.jpg" },
+  { name: 'Dirt Car',price: 2500, color: "blue", img: "assets/voiture3.jpg" },
+  { name: 'Mitsubishi',price: 22000, color: "grey", img: "assets/voiture4.jpg" }
 ];
-
-function card(nb, color, marque) {
-  let html = ``;
-  for (let index = 0; index < nb; index++) {
-    if (car[index].color == color || color == false) {
-      if (car[index].name == marque || marque == false) {
-          html += `
-          <div class="car-card">
-              <img src="assets/voiture${index+1}.jpg" class="car-img">
-              <div class="car-title">${car[index].name}</div>
-              <div class="car-location">Orléans</div>
-              <div class="car-price">${car[index].price}$</div>
-              <form><button type='submit' class="offer-submit"><a href="priceoffer.html">Consulter l'offre</a></button></form>
-          </div>`;
-      }
-    }
-  }
-  document.getElementById('cars').innerHTML = html;
-}
-
-const fs = require('fs');
-const path = require('path');
-
-function uploadFile(file) {
-    if (!file || file.error !== 0) {
-        return false;
-    }
-
-    const uploadDir = path.join(__dirname, 'assets');
-
-    if (!fs.existsSync(uploadDir)) {
-        fs.mkdirSync(uploadDir, { recursive: true });
-    }
-
-    const filename = 'voiture.jpg';
-    const uploadPath = path.join(uploadDir, filename);
-
-    try {
-        fs.renameSync(file.tmp_name, uploadPath);
-        return filename;
-    } catch (err) {
-        console.error(err);
-        return false;
-    }
-}
